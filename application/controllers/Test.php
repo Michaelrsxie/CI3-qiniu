0baf6cef7a57234b2f02aad05cc99a4e43031b27
Michael锐生 <michaelrs.xie@gmail.com>
2017-09-19 18:40:08 +0800

    添加上传文件支持

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,17 +3,32 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 
-    function __construct() {
+    public function __construct()
+    {
         parent::__construct();
 
+		$this->load->library('qiniu');
         $this->load->helper('qiniu_helper');
+
         $this->output->enable_profiler();
     }
 
-	public function index() {
-		$this->load->library('qiniu');
-		$fileName = str_replace('.', '', microtime(TRUE)).'.jpg';
-		$res = $this->qiniu->putFile($fileName, FCPATH.'static/img/weixin-qrcode.jpg');
+	public function index()
+	{
+		$res = $this->qiniu->putFile(FCPATH.'static/img/weixin-qrcode.jpg');
+
+		var_dump($res);
+		var_dump(qiniu_url($res['key']));
+	}
+
+	public function upload()
+	{
+		$this->load->view('upload.html');
+	}
+
+	public function upload_img()
+	{
+		$res = $this->qiniu->uploadFile('image');
 
 		var_dump($res);
 		var_dump(qiniu_url($res['key']));
--- application/libraries/qiniu.php
+++ application/libraries/qiniu.php
@@ -10,7 +10,7 @@ use Qiniu\Storage\BucketManager;
 /**
  * 七牛
  */
-class qiniu
+class Qiniu
 {
 	/**
      * CodeIgniter instance
@@ -39,11 +39,34 @@ class qiniu
 	}
 
 	/**
-	 * 上传图片(路径)
-	 * @param $fileName 上传后的文件名
-	 * @param $file     上传的文件地址
+	 * 获取错误信息
 	 */
-	public function putFile($fileName, $filePath)
+	public function getErrorMsg()
+	{
+		return $this->errorMsg;
+	}
+
+	public function makeFileName()
+	{
+		return str_replace('.', '-', microtime(TRUE));
+	}
+
+	/**
+	 * 获取文件扩展名
+	 */
+	public function getExtension($fileName)
+	{
+		$x = explode('.', $fileName);
+
+		if (count($x) === 1)
+		{
+			return '';
+		}
+
+		return '.'.end($x);
+	}
+
+	protected function upload($fileName, $filePath)
 	{
 		$uploadMgr = new UploadManager();
 		list($ret, $err) = $uploadMgr->putFile($this->token, $fileName, $filePath);
@@ -57,10 +80,45 @@ class qiniu
 	}
 
 	/**
-	 * 获取错误信息
+	 * 上传图片(路径)
+	 * @param $file 上传的文件地址
 	 */
-	public function getErrorMsg()
+	public function putFile($filePath, $newName = FALSE)
 	{
-		return $this->errorMsg;
+		if ( ! $newName)
+		{
+			$fileName = $this->makeFileName().$this->getExtension($filePath);
+		}
+		else
+		{
+			$fileName = $newName;
+		}
+
+		return $this->upload($fileName, $filePath);
+	}
+
+	/**
+	 * 上传图片(表单)
+	 * @param $file 上传的文件名
+	 */
+	public function uploadFile($file, $newName = FALSE)
+	{
+		if (isset($_FILES[$file]) && $_FILES[$file]['size'] > 0)
+		{
+			if ( ! $newName)
+			{
+				$fileName = $this->makeFileName().$this->getExtension($_FILES[$file]['name']);
+			}
+			else
+			{
+				$fileName = $newName;
+			}
+
+			$filePath = $_FILES[$file]['tmp_name'];
+			return $this->upload($fileName, $filePath);
+		}
+
+		$this->errorMsg = '文件未找到';
+		return FALSE;
 	}
 }
